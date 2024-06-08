394d93c8d24bda37da5847759a61939aab18e20e
AvnishRana25 <avnishrana797@gmail.com>
2024-06-09 00:17:40 +0530

    second commit

---
+++ half.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int x; //declring for x
+    cout << "enter a number"; //printing input line
+    cin >> x;  // taking input 
+    float c = x/2;
+    cout << c;  //printing output
+    return 0;
+}
+
+
+
+    
+
---
+++ test.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n = 0 ;
+    cout << "Enter: ";
+    cin >> n ; 
+    cout << "The value of n is:" << n << endl;
+    return 0;
+}
